0ecc87d629e50cf7424dd241c02557dce8863e00
Saqlain Dawood <saqlaindawood123@gmail.com>
2026-07-25 23:17:39 -0700

    update the frontend url

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 // frontend/src/api/apiClient.js
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
-
+console.log("API BASE URL:", API_BASE_URL);
 class ApiClient {
   constructor() {
     this.baseURL = API_BASE_URL;
